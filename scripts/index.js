a995683a570c5a1c67c4c04179463cbebae9e814
NyctibiusVII <matheus.dev.07@gmail.com>
2020-12-21 07:11:20 -0300

    class 3 of the project

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+function start() {
+    $("#startGame").hide();
+    
+	$("#backgroundGame").append("<div id='player'></div>");
+	$("#backgroundGame").append("<div id='enemy1'></div>");
+	$("#backgroundGame").append("<div id='enemy2'></div>");
+	$("#backgroundGame").append("<div id='friend'></div>");
+}
