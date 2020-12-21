d742c45868fa4302647e1d2d6ddbe3efc0c478f4
NyctibiusVII <matheus.dev.07@gmail.com>
2020-12-21 07:56:20 -0300

    class 5 of the project

--- scripts/index.js
+++ scripts/index.js
@@ -5,4 +5,20 @@ function start() {
 	$("#backgroundGame").append("<div id='enemy1' class='enemy1AnimationClass'></div>");
 	$("#backgroundGame").append("<div id='enemy2'></div>");
 	$("#backgroundGame").append("<div id='friend' class='friendAnimationClass'></div>");
+    
+    var game = {}
+    const interval = game.interval = 30;
+    const velocity = game.velocity = 1;
+    var backgroundDirection = game.backgroundDirection = "left";
+      
+    game.timer = setInterval(loop, interval);
+
+    function loop() {
+        backgroundAnimation();
+    }
+	
+	function backgroundAnimation() {
+        backgroundDirection = parseInt($("#backgroundGame").css("background-position"));
+        $("#backgroundGame").css("background-position", backgroundDirection - velocity);
+    }
 }
