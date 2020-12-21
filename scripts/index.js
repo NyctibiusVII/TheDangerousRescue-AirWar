6e3d3d662ca2351255d403251014838f53dec277
NyctibiusVII <matheus.dev.07@gmail.com>
2020-12-21 10:17:03 -0300

    class 7 of the project

--- scripts/index.js
+++ scripts/index.js
@@ -7,12 +7,21 @@ function start() {
 	$("#backgroundGame").append("<div id='friend' class='friendAnimationClass'></div>");
 
     var game = {
-        interval: 30,
-        velocity: 1,
+        intervalLoop: 30,
+        topLimit: 0,
+        bottomLimit: 434,
+        
         backgroundDirection: "left",
-        amountOfPixelsMoved: 10
+        velocityOfBackground: 1,
+        movingFrom_n_To_n_pixels: 10,
+        
+        directionEnemy: "left",
+        enemyVelocity: 5,
+        enemyPositionX: undefined,
+        enemyPositionY: parseInt(Math.random() * 334),
+        startingPositionOnTheEnemy: 694,
     }
-    game.timer = setInterval(loop, game.interval);
+    game.timer = setInterval(loop, game.intervalLoop);
 
     var keyboardKey = {
         W: 87,
@@ -26,24 +35,45 @@ function start() {
 
     function loop() {
         movePlayer();
+        moveEnemy1();
         backgroundAnimation();
     }
 
     function movePlayer() {
         if (game.pressed[keyboardKey.W]) {
-            var topo = parseInt($("#player").css("top"));
-            $("#player").css("top",topo - game.amountOfPixelsMoved);
+            let top = parseInt($("#player").css("top"));
+            $("#player").css("top",top - game.movingFrom_n_To_n_pixels);
+
+            if (top <= game.topLimit) {
+                $("#player").css("top",top + game.movingFrom_n_To_n_pixels);
+            }
         }
         if (game.pressed[keyboardKey.S]) {
-            var topo = parseInt($("#player").css("top"));
-            $("#player").css("top",topo + game.amountOfPixelsMoved);
+            let top = parseInt($("#player").css("top"));
+            $("#player").css("top",top + game.movingFrom_n_To_n_pixels);
+
+            if (top >= game.bottomLimit) {
+                $("#player").css("top",top - game.movingFrom_n_To_n_pixels);
+            }
         }
         if (game.pressed[keyboardKey.D]) {
         }
     }
 
+    function moveEnemy1() {
+        game.enemyPositionX = parseInt($("#enemy1").css(game.directionEnemy));
+        $("#enemy1").css(game.directionEnemy,game.enemyPositionX - game.enemyVelocity);
+        $("#enemy1").css("top",game.enemyPositionY);
+        
+        if (game.enemyPositionX <= 0) {
+            game.enemyPositionY = parseInt(Math.random() * 334);
+            $("#enemy1").css(game.directionEnemy, game.startingPositionOnTheEnemy);
+            $("#enemy1").css("top",game.enemyPositionY);
+        }
+    }
+
 	function backgroundAnimation() {
         game.backgroundDirection = parseInt($("#backgroundGame").css("background-position"));
-        $("#backgroundGame").css("background-position", game.backgroundDirection - game.velocity);
+        $("#backgroundGame").css("background-position", game.backgroundDirection - game.velocityOfBackground);
     }
 }
