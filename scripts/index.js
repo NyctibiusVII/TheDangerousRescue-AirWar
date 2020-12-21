e9b9ae9d2395563bca7eca587e0034063c91c2f1
NyctibiusVII <matheus.dev.07@gmail.com>
2020-12-21 07:41:54 -0300

    class 4 of the project

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,8 @@
 function start() {
     $("#startGame").hide();
     
-	$("#backgroundGame").append("<div id='player'></div>");
-	$("#backgroundGame").append("<div id='enemy1'></div>");
+	$("#backgroundGame").append("<div id='player' class='playerAnimationClass'></div>");
+	$("#backgroundGame").append("<div id='enemy1' class='enemy1AnimationClass'></div>");
 	$("#backgroundGame").append("<div id='enemy2'></div>");
-	$("#backgroundGame").append("<div id='friend'></div>");
+	$("#backgroundGame").append("<div id='friend' class='friendAnimationClass'></div>");
 }
